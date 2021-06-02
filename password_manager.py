097b3c7f998a7bd484520ee6695fece69b737824
Fernando Josue Campos Enriquez <fernandojosuece@gmail.com>
2021-06-02 07:40:16 -0500

    Update password_manager.py

--- password_manager.py
+++ password_manager.py
@@ -1,46 +1,33 @@
 from hashlib import sha256
 
 
-ADMIN_PASSWORD = "Your secret admin password"
+def create_password(service, master_key):
+    return sha256(master_key.encode('utf-8') + service.lower().encode('utf-8')).hexdigest()[:10]
 
-def create_password(service, admin_pass):
-    return sha256(admin_pass.encode('utf-8') + service.lower().encode('utf-8')).hexdigest()[:10]
+def get_password(master_key, service):
+    return create_password(service, master_key)
 
-def get_hex_key(admin_pass, service):
-    return sha256(admin_pass.encode('utf-8') + service.lower().encode('utf-8')).hexdigest()
+def add_password(service, master_key):
+    return create_password(service, master_key)
 
-def get_password(admin_pass, service):
-    return create_password(service, admin_pass)
-
-def add_password(service, admin_pass):
-    return create_password(service, admin_pass)
-
-
+print("\n¡Bienvenido! ¿Qué deseas hacer?\n")
 print("\n"+ "*"*15)
 print("Comandos:")
 print("s = salir del programa")
 print("oc = obtener contraseña")
 print("gc = generar contraseña")
 print("*"*15)
-
-connect = ""
-
-while connect != ADMIN_PASSWORD:
-    connect = input("\n¿Cuál es tu contraseña de administrador?\n")
-    if connect == "s":
-        break
-
-if connect == ADMIN_PASSWORD:
-    print("\n¡Bienvenido! ¿Qué deseas hacer?\n")
     
-    while True:
-        input_ = input(":")
+while True:
+    input_ = input(":")
 
-        if input_ == "s":
-            break
-        if input_ == "gc":
-            service = input("¿Cuál es el nombre del servicio?\n")
-            print("\n" + "La contraseña creada para " + service.capitalize() + " es:\n" + add_password(service, ADMIN_PASSWORD))
-        if input_ == "oc":
-            service = input("\n¿Cuál es el nombre del servicio?\n")
-            print("\n" + "Tu contraseña de " + service.capitalize() + " es:\n" + get_password(ADMIN_PASSWORD, service))
+    if input_ == "s":
+        break
+    if input_ == "gc":
+        master_key = input("\n¿Cuál es tu llave maestra?\n")
+        service = input("\n¿Cuál es el nombre del servicio?\n")
+        print("\n" + "La contraseña creada para " + service.capitalize() + " es:\n" + add_password(service, master_key))
+    if input_ == "oc":
+        master_key = input("\n¿Cuál es tu llave maestra?\n")
+        service = input("\n¿Cuál es el nombre del servicio?\n")
+        print("\n" + "Tu contraseña de " + service.capitalize() + " es:\n" + get_password(master_key, service))
